3045c691fd0888bdb811db57d3df7f994dc5a356
romainmannino <romainmannino@gmail.com>
2025-07-28 14:26:12 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Bienvenue sur MatchLive</h1>
+      <p>Match test : OL - PSG (à venir)</p>
+    </div>
+  );
+}
+
+export default App;
