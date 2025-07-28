c59b4f72a55d115bac6c2b324fa765220bc383ae
romainmannino <romainmannino@gmail.com>
2025-07-28 14:28:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-// App.js
-import React from "react";
+const style = {
+  padding: "2rem",
+  fontFamily: "sans-serif"
+};
 
 function App() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+    <div style={style}>
       <h1>Bienvenue sur MatchLive</h1>
       <p>Match test : OL - PSG (à venir)</p>
     </div>
   );
 }
-
-export default App;
